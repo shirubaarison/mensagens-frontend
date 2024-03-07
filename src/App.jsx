9fe95e1f72cd03145124d525dc5d94c5e0af7919
Alysson Silva <shirubaarison@gmail.com>
2024-03-07 19:23:10 -0300

    alterar forma de guardar informações do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,12 @@ import DeliciasLogo from './assets/delicias.png'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import Mensagens from './components/Mensagens'
+import MensagemForm from './components/MensagemForm'
 import mensagensService from './services/mensagensService'
 
+import Menu from './components/Menu'
+
+// TODO: melhorar essa merda
 const BigLogo = () => {
   return (
     <div className='mainLogo'>
@@ -17,14 +21,30 @@ const BigLogo = () => {
 
 const App = () => {
   const [ mensagens, setMensagens ] = useState([])
+  const [ user, setUser ] = useState('')
+
   const ws = useRef(null)
+
+  useEffect(() => {
+    const usuarioJSON = window.localStorage.getItem('usuarioMensagensApp')
+    let usuario
+    if (usuarioJSON) {
+      usuario = JSON.parse(usuarioJSON)
+      setUser(usuario)
+    }
+  }, [])
 
   useEffect(() => {
     mensagensService.getAll().then(mensagens => setMensagens(mensagens));
     
-    ws.current = new WebSocket('wss://mensagens.onrender.com:')
-    ws.current.onopen = () => console.log("ws opened")
-    ws.current.onclose = () => console.log("ws closed")
+    // quando buildar use esse endereço de websocket
+    // ws.current = new WebSocket('wss://mensagens.onrender.com')
+
+    ws.current = new WebSocket('ws://localhost:3002')
+
+    ws.current.onopen = () => console.log("ws conexao estabelecida")
+  
+    ws.current.onclose = () => console.log('ws fechado')
 
     const wsCurrent = ws.current
   
@@ -35,19 +55,22 @@ const App = () => {
 
   useEffect(() => {
     if (!ws.current) return
-
+  
     ws.current.onmessage = (event) => {
       const message = JSON.parse(event.data)
       const tempId = Date.now().toString()
       const newMessage = {...message, id: tempId}
-      // ver se o newMessage causa o bug, acho que nao
+
+      // ver se o newMessage causa o bug, acho que nao (precisa de ID)
       setMensagens((prevMensagens) => [...prevMensagens, newMessage])
     }
   }, [])
 
   const submitMensagem = async (men) => {
     const menString = JSON.stringify(men)
-    
+    const parsedString = JSON.parse(menString)
+    if (parsedString.mensagem === '') return
+
     try {
       if (ws.current && menString.trim() !== '') {
         ws.current.send(menString)
@@ -55,7 +78,7 @@ const App = () => {
 
       const response = await mensagensService.enviarMensagem(men)
       setMensagens(mensagens.concat(response))
-
+      
     } catch (error) {
       console.log(error)
     }
@@ -66,17 +89,36 @@ const App = () => {
       await mensagensService.deletarMensagem(id)
       
       setMensagens(mensagens.filter(m => m.id !== id))
-
     } catch (error) {
       console.log(error)
     }
-    
+  }
+
+  const deletarUsername = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+  // buggy code
+  const deletarTudo = async () => {
+    try {
+      // ele tira o autor ai precisa refazer o nome dnv (?)
+      await mensagensService.deletarTudo() 
+      setMensagens([]) 
+      setUser('') // lol, assim funciona pois é obrigatorio digitar dnv 
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
     <div className='main'>
-      <BigLogo />
-      <Mensagens mensagens={mensagens} submitMensagem={submitMensagem} deleteMensagem={deleteMensagem} />
+      <BigLogo /> 
+      <div className='container'>
+        <Mensagens mensagens={mensagens} deleteMensagem={deleteMensagem} usuario={user} />
+        <MensagemForm submitMensagem={submitMensagem} usuario={user} setUsuario={setUser}/>
+        <Menu deletarUsername={deletarUsername} usuario={user} deletarTudo={deletarTudo}/>
+      </div>
     </div>
   )
 }
