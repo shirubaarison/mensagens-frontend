5ca7179aa81d4c0265c2487992a6055fdedf1e60
Alysson Silva <shirubaarison@gmail.com>
2024-04-25 20:09:06 -0300

    adicionar nova lógica do websocket

---
+++ src/middleware/socketMiddleware.js
@@ -0,0 +1,32 @@
+import { adicionarMensagem } from "../reducers/mensagemReducer"
+
+export const socketMiddleware = (socket) => (params) => (next) => (action) => {
+    const { dispatch } = params
+    const { type, payload } = action
+
+    switch (type) {
+    case 'socket/connect':
+        socket.connect('ws://localhost:3002')
+
+        socket.on('open', () => { console.log('conexão estabelecida com websocket')})
+        socket.on('message', (data) => { 
+            const mensagem = JSON.parse(data.data)
+            dispatch(adicionarMensagem(mensagem))
+        })
+        socket.on('close', () => { console.log('adeus') })
+        break
+
+    case 'socket/disconnect':
+        socket.disconnect()
+        break
+    
+    case 'socket/send':
+        socket.send(payload)
+        break
+    
+    default:
+        break
+    }
+
+    return next(action)
+}
